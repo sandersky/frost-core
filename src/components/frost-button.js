8e95be34532226582affa87a5ea2e66b14bf8999
Matthew Dahl <mdahl@ciena.com>
2017-01-13 15:00:36 -0800

    Add SASS for components and priority attribute to button

--- src/components/frost-button.js
+++ src/components/frost-button.js
@@ -1,9 +1,19 @@
+export const priorities = [
+  'large',
+  'medium',
+  'small'
+]
+
 export default class FrostButton extends HTMLButtonElement {
   createdCallback () {
-    const text = this.getAttribute('text')
+    const priority = this.getAttribute('priority')
 
-    if (text) {
-      this.prepend(text)
+    if (!priority) {
+      this.setAttribute('priority', 'medium')
+    } else if (priorities.indexOf(priority) === -1) {
+      throw new Error(
+        `Invalid priority "${priority}" for frost-button. Expected one of: ${priorities.join(', ')}`
+      )
     }
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,22 @@
 module.exports = {
   entry: './src/index.js',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          plugins: [
+            'transform-export-extensions',
+            'transform-es2015-modules-umd'
+          ],
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
   output: {
-    filename: 'dist/frost-core.min.js'
+    filename: 'dist/frost-core.js'
   }
 }
