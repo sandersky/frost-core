3b38913e34d2a553aabf9c6718caae65861598ab
Matthew Dahl <mdahl@ciena.com>
2017-01-13 23:28:07 -0800

    Minor code cleanup

--- src/components/frost-code-example.js
+++ src/components/frost-code-example.js
@@ -2,6 +2,29 @@ import Prism from 'prismjs'
 
 import {PropTypes, createComponent} from '../react'
 
+/**
+ * Trim superfluous leading whitespace from text so all content is shifted to
+ * left edge when renderered in a <pre> tag. Essentially the same thing as
+ * pressing Shift + Tab multiple times in a modern text editor to get the
+ * content next to the left gutter.
+ * @param {String} text - text to trim
+ * @returns {String} trimmed text
+ */
+function trimSuperfluousLeadingWhitespace (text) {
+  const lines = text.split('\n')
+
+  const trimCount = lines
+    .map((line) => line.match(/(\s+)[^\s]/)) // Find leading whitespace
+    .filter((matches) => matches !== null) // Filter out lines w/o leading whitepsace
+    .map((matches) => matches[1]) // Get leading whitespace (ignoring 1st non whitespace character)
+    .map((leadingWhitespace) => leadingWhitespace.length) // Get count of leading whitespace chars
+    .reduce((a, b) => Math.min(a, b), Infinity) // Get superfluous leading whitespace count
+
+  return lines
+    .map((line) => line.substring(trimCount)) // Remove superfluous leading whitespace from every line
+    .join('\n').trim()
+}
+
 class FrostCodeExampleBase extends HTMLDivElement {
   get propTypes () {
     return {
@@ -10,20 +33,8 @@ class FrostCodeExampleBase extends HTMLDivElement {
   }
 
   componentWillMount () {
-    const lines = this.innerHTML.split('\n')
-
-    const trimCount = lines
-      .map((line) => line.match(/(\s+)[^\s]/))
-      .filter((matches) => matches !== null)
-      .map((matches) => matches[1])
-      .map((leadingWhitespace) => leadingWhitespace.length)
-      .reduce((a, b) => Math.min(a, b), Infinity)
-
-    const trimmedLines = lines
-      .map((line) => line.substring(trimCount))
-
     this.setState({
-      example: trimmedLines.join('\n').trim()
+      example: trimSuperfluousLeadingWhitespace(this.innerHTML)
     })
   }
 
