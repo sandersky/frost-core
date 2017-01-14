3b0c2badbc90693227b0c95fd0043d4e6c18027d
Matthew Dahl <mdahl@ciena.com>
2017-01-13 23:57:27 -0800

    Commit less to demo

--- scripts/publish-gh-pages.sh
+++ scripts/publish-gh-pages.sh
@@ -10,6 +10,6 @@ cd ${TMP_GH_PAGES_DIR}
 git checkout gh-pages
 git rm -rf *
 cp -r ../demo/* .
-git add --all
+git add *.*
 git commit -m "[ci skip] Automated gh-pages commit of ${VERSION}"
 git push origin gh-pages > /dev/null 2>&1
