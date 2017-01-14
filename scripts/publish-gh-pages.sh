b3716b77e9d83a07ddf81d699a95bc18b100bb43
Matthew Dahl <mdahl@ciena.com>
2017-01-14 00:17:47 -0800

    More cleanup

--- scripts/publish-gh-pages.sh
+++ scripts/publish-gh-pages.sh
@@ -5,9 +5,8 @@ VERSION=`node -e "console.log(require('./package.json').version)"`
 npm run build-demo
 git checkout gh-pages
 git rm -rf *
-rm ./.gitignore ./*.css ./*.js ./*.html ./*.md
 cp -r ./demo/* .
-git add ./*.css ./*.js ./*.html
+git add frost-core.css frost-core.js index.html prism-theme.js
 git commit -m "[ci skip] Automated gh-pages commit of ${VERSION}"
 git push origin gh-pages
 git checkout master
