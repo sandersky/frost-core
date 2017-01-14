262937e21d94235a1f828665ec4781a0e449bcfc
Matthew Dahl <mdahl@ciena.com>
2017-01-14 00:11:36 -0800

    more cleanup

--- scripts/publish-gh-pages.sh
+++ scripts/publish-gh-pages.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 VERSION=`node -e "console.log(require('./package.json').version)"`
-TMP_GH_PAGES_DIR=.gh-pages-demo
 
 npm run build-demo
 git checkout gh-pages
 git rm -rf *
-rm .gitignore *.css *.js *.html *.md
+rm ./.gitignore ./*.css ./*.js ./*.html ./*.md
 cp -r ./demo/* .
-git add *.css *.js *.html
+git add ./*.css ./*.js ./*.html
 git commit -m "[ci skip] Automated gh-pages commit of ${VERSION}"
 git push origin gh-pages
 git checkout master
