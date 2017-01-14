5a0b598a82bbe61c6531c7d21f3f9589b7792457
Matthew Dahl <mdahl@ciena.com>
2017-01-14 00:02:37 -0800

    Another attempt to fix publish script

--- scripts/publish-gh-pages.sh
+++ scripts/publish-gh-pages.sh
@@ -4,12 +4,11 @@ VERSION=`node -e "console.log(require('./package.json').version)"`
 TMP_GH_PAGES_DIR=.gh-pages-demo
 
 npm run build-demo
-rm -rf ${TMP_GH_PAGES_DIR}
-mkdir ${TMP_GH_PAGES_DIR}
-cd ${TMP_GH_PAGES_DIR}
 git checkout gh-pages
 git rm -rf *
+rm .gitignore *.css *.js *.html *.md
 cp -r ../demo/* .
-git add *.*
+rm -rf demo scripts src tests
+git add --all
 git commit -m "[ci skip] Automated gh-pages commit of ${VERSION}"
 git push origin gh-pages > /dev/null 2>&1
