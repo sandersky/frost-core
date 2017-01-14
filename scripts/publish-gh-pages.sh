851ec9a0e5c8b107cc3a9a6b09ba2870df4e0220
Matthew Dahl <mdahl@ciena.com>
2017-01-14 00:29:58 -0800

    Fix bug in script

--- scripts/publish-gh-pages.sh
+++ scripts/publish-gh-pages.sh
@@ -7,7 +7,7 @@ cp -r ./demo/ ./tmp
 git checkout gh-pages
 git rm -rf *
 cp -r ./tmp/* .
-git add frost-core.css frost-core.js index.html prism-theme.js
+git add frost-core.css frost-core.js index.html prism-theme.css
 git commit -m "[ci skip] Automated gh-pages commit of ${VERSION}"
 git push origin gh-pages
 git checkout master
