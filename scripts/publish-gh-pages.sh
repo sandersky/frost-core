6722206a97ef28b72a0fab956d16f04137bdda16
Matthew Dahl <mdahl@ciena.com>
2017-01-14 00:23:49 -0800

    Try another one

--- scripts/publish-gh-pages.sh
+++ scripts/publish-gh-pages.sh
@@ -3,9 +3,10 @@
 VERSION=`node -e "console.log(require('./package.json').version)"`
 
 npm run build-demo
+cp -r ./demo/ ./tmp
 git checkout gh-pages
 git rm -rf *
-cp -r ./demo/* .
+cp -r ./tmp/* .
 git add frost-core.css frost-core.js index.html prism-theme.js
 git commit -m "[ci skip] Automated gh-pages commit of ${VERSION}"
 git push origin gh-pages
